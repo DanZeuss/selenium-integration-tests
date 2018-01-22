e833adbcc1cf14e80c903fb41c108e8827bfba0a
danzeuss <danvc@zeuss.com>
2018-01-22 17:02:11 -0200

    Adding new packages to use Jest

--- selenium-tests.js
+++ selenium-tests.js
@@ -1,15 +1,8 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 var webdriver = require('selenium-webdriver');
-// var driver = new webdriver.Builder()
-//   // defines the host of the current HUB
-//   .usingServer('http://localhost:4444/wd/hub')
-//   // the definition of the browser is done by running the scripts "run-test-*.sh" that loads/exports the variable that defines the 
-//   // current browser
-//   .forBrowser()
-//   .build();
  
-  (async function example() {
-    let driver = new Builder()
+  (async function LogonTest() {
+    let browser = new Builder()
       // defines the host of the current HUB
       .usingServer('http://192.168.1.6:4444/wd/hub')
       // the definition of the browser is done by running the scripts "run-test-*.sh" that loads/exports the variable that defines the 
@@ -18,23 +11,26 @@ var webdriver = require('selenium-webdriver');
       .build();
 
     try {
-      await driver.get('https://troy.zeuss.com/');
-      await driver.getTitle().then(title => {
+      await browser.get('https://troy.zeuss.com/');
+      await browser.getTitle().then(title => {
         console.log(title);
       });
 
-      await driver.findElement(By.id("username")).sendKeys("zeuss_admin");
-      await driver.findElement(By.id("password")).sendKeys("zeuss_admin");
-      await driver.findElement(By.id("btn_signin")).click().then();
-      await driver.takeScreenshot().then(
+      await browser.findElement(By.id("username")).sendKeys("zeuss_admin");
+      await browser.findElement(By.id("password")).sendKeys("zeuss_admin");
+      await browser.findElement(By.id("btn_signin")).click().then();
+      await browser.findElement(By.className("username")).getText().then((userLogged) => {
+        console.info(userLogged.includes("Zeuss Admin"))
+      })
+      await browser.takeScreenshot().then(
         function(image, err) {
             require('fs').writeFile('out.png', image, 'base64', function(err) {
-                console.log(err);
+                //console.log(image);
             });
         }
     );
-      // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    
     } finally {
-      await driver.quit();
+      await browser.quit();
     }
   })();
